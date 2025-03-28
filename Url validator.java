a1123c93bedfa0ab7849712d550eb8d2db75736f
Praveen Kumar <86505091+praveen-221@users.noreply.github.com>
2025-03-28 10:46:53 +0530

    Update sql

--- Url validator.java
+++ Url validator.java
@@ -189,3 +189,61 @@ SELECT DISTINCT
 FROM RecursiveDependencies rd
 WHERE rd.ObjectType IN ('USER_TABLE', 'SQL_SCALAR_FUNCTION', 'SQL_TABLE_VALUED_FUNCTION', 'VIEW', 'SYNONYM')
 ORDER BY rd.ObjectType, rd.ObjectName;
+
+----------
+// V3
+DECLARE @ViewOrProcedureName NVARCHAR(255) = 'YourViewOrProcedureName'; -- Replace with your view or procedure name
+DECLARE @SQL NVARCHAR(MAX);
+
+WITH RecursiveDependencies AS (
+    -- Step 1: Get Direct Dependencies (Tables, Views, Functions, Synonyms)
+    SELECT 
+        dep.referenced_id AS ObjectID, 
+        obj.name AS ObjectName,
+        obj.type_desc AS ObjectType,
+        syn.base_object_name AS SynonymBaseObject,
+        CASE 
+            WHEN syn.base_object_name LIKE '%.%' THEN PARSENAME(syn.base_object_name, 3) 
+            ELSE DB_NAME() 
+        END AS ReferencedDB, -- Extracts DB name if exists
+        CASE 
+            WHEN syn.base_object_name LIKE '%.%' THEN PARSENAME(syn.base_object_name, 2) 
+            ELSE SCHEMA_NAME(obj.schema_id) 
+        END AS ReferencedSchema
+    FROM sys.sql_expression_dependencies dep
+    INNER JOIN sys.objects obj ON dep.referenced_id = obj.object_id
+    LEFT JOIN sys.synonyms syn ON obj.name = syn.name  -- Resolving synonyms
+    WHERE dep.referencing_id = OBJECT_ID(@ViewOrProcedureName)
+
+    UNION ALL
+
+    -- Step 2: Recursively Traverse Views and Synonyms
+    SELECT 
+        dep.referenced_id,
+        obj.name,
+        obj.type_desc,
+        syn.base_object_name,
+        CASE 
+            WHEN syn.base_object_name LIKE '%.%' THEN PARSENAME(syn.base_object_name, 3) 
+            ELSE DB_NAME() 
+        END AS ReferencedDB, -- Extract DB name
+        CASE 
+            WHEN syn.base_object_name LIKE '%.%' THEN PARSENAME(syn.base_object_name, 2) 
+            ELSE SCHEMA_NAME(obj.schema_id) 
+        END AS ReferencedSchema
+    FROM sys.sql_expression_dependencies dep
+    INNER JOIN sys.objects obj ON dep.referenced_id = obj.object_id
+    LEFT JOIN sys.synonyms syn ON obj.name = syn.name
+    INNER JOIN RecursiveDependencies rd ON dep.referencing_id = rd.ObjectID
+    WHERE obj.type_desc IN ('VIEW', 'SYNONYM') -- Keep resolving views and synonyms
+)
+-- Step 3: Select Base Tables, Views, Functions, Synonyms (Including External DB References)
+SELECT DISTINCT 
+    rd.ObjectName, 
+    rd.ObjectType,
+    COALESCE(rd.SynonymBaseObject, 'Direct Reference') AS ResolvedBaseObject,
+    rd.ReferencedDB AS DatabaseName,
+    rd.ReferencedSchema AS SchemaName
+FROM RecursiveDependencies rd
+WHERE rd.ObjectType IN ('USER_TABLE', 'SQL_SCALAR_FUNCTION', 'SQL_TABLE_VALUED_FUNCTION', 'VIEW', 'SYNONYM')
+ORDER BY rd.DatabaseName, rd.SchemaName, rd.ObjectName;
